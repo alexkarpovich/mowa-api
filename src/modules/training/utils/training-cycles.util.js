2e906f823b913224c7c8a5f7b26c94492c1ee48b
Aliaksandr Karpovich <alexsure.k@gmail.com>
2020-02-06 15:23:55 +0300

    Training cycles updates

--- src/modules/training/utils/training-cycles.util.js
+++ src/modules/training/utils/training-cycles.util.js
@@ -1,4 +1,7 @@
+const { chunk, shuffle } = require('lodash');
 const Training = require('./training.util');
+
+const MIN_CHUNK_SIZE = 7;
 
 class TrainingCycles extends Training {
   constructor(driver, setIds) {
@@ -6,10 +9,21 @@ class TrainingCycles extends Training {
   }
 
   async countTranslations(session, trainingId) {
-    return session.run(`
+    const { records } = session.run(`
         MATCH (train:Training{id: $trainingId})-[:INCLUDES]->(s:Set)-[:INCLUDE]->(trans:Translation)
         RETURN COUNT(DISTINCT trans) as count
       `, { trainingId });
+
+    return +records[0].get('count');
+  }
+
+  async getTranslationIds(session, trainingId) {
+    const { records } = session.run(`
+      MATCH (train:Training{id: $trainingId})-[:INCLUDES]->(s:Set)-[:INCLUDES]->(trans:Translation)
+      WITH DISTINCT trans.id as id
+    `, { trainingId });
+
+    return records.map(rec => rec.get('id'));
   }
 
   async initialize() {
@@ -23,10 +37,22 @@ class TrainingCycles extends Training {
       }
 
       training = await this.assignSets(session);
-      const count = await this.countTranslations(session, training.id);
-      const stageCount = Math.round(Math.log(count / 7.) / Math.log(2));
+      const transIds = await this.getTranslationIds(session, training.id);
+      const count = transIds.length;
+      const stageCount = Math.round(Math.log(count * 1. / MIN_CHUNK_SIZE) / Math.log(2));
+      let stages = Array.from(Array(stageCount).keys());
 
-      console.log(stageCount);
+      console.log(count, stageCount);
+      let ids, chunkSize;
+
+      stages = stages.map((k) => {
+        ids = shuffle(transIds);
+        chunkSize = count / (MIN_CHUNK_SIZE * Math.pow(2, k));
+
+        return chunkSize ? chunk(ids, chunkSize) : ids;
+      });
+
+      console.log(stages);
 
       return training;
     } catch (err) {
