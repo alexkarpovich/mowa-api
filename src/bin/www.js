667ca3a8f76f6bfd8c4cea96d9cb4786a948a03d
Aliaksandr Karpovich <alexsure.k@gmail.com>
2020-03-22 12:16:46 +0300

    Detaching rabbitmq logic

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,21 +1,23 @@
-const amqp = require('amqplib/callback_api');
+//const amqp = require('amqplib/callback_api');
 
 const config = require('../config');
 const app = require('../libs/app');
-const { initProducer } = require('../libs/producer');
+//const { initProducer } = require('../libs/producer');
 
 const port = config.get('port') || 4000;
 
-amqp.connect(config.get('rabbitmq:uri'), function (err, conn) {
-  if (err) {
-    throw new Error(err);
-  }
-
-  if (process.env.NODE_ENV === 'development') {
-    initProducer(app, conn);
-  }
-
-  app.listen({port}, () => {
-    console.log(`🚀  Server ready at http://localhost:${port}`);
-  });
+app.listen({port}, () => {
+  console.log(`🚀  Server ready at http://localhost:${port}`);
 });
+
+// amqp.connect(config.get('rabbitmq:uri'), function (err, conn) {
+//   if (err) {
+//     throw new Error(err);
+//   }
+//
+//   initProducer(app, conn);
+//
+//   app.listen({port}, () => {
+//     console.log(`🚀  Server ready at http://localhost:${port}`);
+//   });
+// });
